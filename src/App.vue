e3568f1d4a6263a010cd3502f972a19f3525b28d
unknownkazah <119103885+unknownkazah@users.noreply.github.com>
2025-04-08 17:56:55 +0500

    update8

--- src/App.vue
+++ src/App.vue
@@ -69,7 +69,7 @@
 <script setup>
 import { reactive, ref, computed } from 'vue'
 
-const API_URL = 'https://unknownqazaq.github.io/eskertuBot/api/tenants'
+const API_URL = 'https://eskertubot.onrender.com/api/tenants'
 
 // Пример данных для демонстрации
 const tenants = ref([
